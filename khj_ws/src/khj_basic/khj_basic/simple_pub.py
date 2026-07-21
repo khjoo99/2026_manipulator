ee5303857a3728f8b4780c75f0ddd387e143953a
khjoo <khjoo990408@gmail.com>
2026-07-21 11:40:37 +0900

    클래스펍 추가

--- khj_ws/src/khj_basic/khj_basic/simple_pub.py
+++ khj_ws/src/khj_basic/khj_basic/simple_pub.py
@@ -1,5 +1,23 @@
-def main():
-    print("첫번째 프로그램입니다.")
+import rclpy
+from rclpy.node import Node
+
+
+def timer_callback():
+    print("첫번째 프로그램")
+
+
+def main(args=None):
+    rclpy.init(args=args)   # rmw 활성화
+    node = Node("message_pub")  # 노드 이름
+    #  timer 등록
+    node.create_timer(1, timer_callback) 
     
+    try:
+        rclpy.spin(node)  # 블럭 (무한 루프)
+    except KeyboardInterrupt:
+        node.get_logger().info("KeyboardInterrupt")
+    finally:
+        node.destroy_node()
+
     if __name__ == '__main__':
         main()
