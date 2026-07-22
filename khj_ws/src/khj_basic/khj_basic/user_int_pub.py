0cf72a2a0680cf5ecf73c8629273a988ac671980
khjoo <khjoo990408@gmail.com>
2026-07-22 13:56:11 +0900

    user_int_pub

--- khj_ws/src/khj_basic/khj_basic/user_int_pub.py
+++ khj_ws/src/khj_basic/khj_basic/user_int_pub.py
@@ -1,33 +1,34 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
-from user_int_msg.msg import UserInt
+from user_interface.msg import UserInt
 
 
 class M_pub(Node):
     def __init__(self):
-        super().__init__("m_pub")  # 노드 이름
-        self.create_timer(1, self.timer_callback)  #  timer 등록
-        self.pub = self.create_publisher(UserInt, "message", 10)  # publisher 등록
-
+        super().__init__("m_pub")
+        self.create_timer(1, self.timer_callback)
+        self.pub = self.create_publisher(UserInt, "message", 10)
 
     def timer_callback(self):
         msg = UserInt()
+        msg.header.frame_id = "time test"
+        msg.header.stamp = self.get_clock().now().to_msg()
         msg.user_int = 12
         msg.user_int2 = 23
-        msg.user_int3 = 32
-        self.pub.publish(msg)
+        msg.user_int3 = 53
+        self.pub.publish(msg)  # DDS 로 보내는 기능 수행
+
 
 def main(args=None):
-    rclpy.init(args=args)   # rmw 활성화
-    node = M_pub()  # 노드 이름
-    
+    rclpy.init(args=args)  # rmw 활성화
+    node = M_pub()
     try:
         rclpy.spin(node)  # 블럭 (무한 루프)
     except KeyboardInterrupt:
-        node.get_logger().info("KeyboardInterrupt")
+        node.get_logger().info("키보드 인터럽트")
     finally:
         node.destroy_node()
 
-    if __name__ == '__main__':
-        main()
+
+if __name__ == "__main__":
+    main()
