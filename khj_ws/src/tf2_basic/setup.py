9748482f25a9a74b1080353955e0316be7a1b209
khjoo <khjoo990408@gmail.com>
2026-07-24 14:57:53 +0900

    urdf

--- khj_ws/src/tf2_basic/setup.py
+++ khj_ws/src/tf2_basic/setup.py
@@ -1,15 +1,21 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
-package_name = 'tf2_basic'
+package_name = "tf2_basic"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob(os.path.join("launch", "*.launch.py"))),
+        ("share/" + package_name + "/urdf", glob(os.path.join("urdf", "*.*"))),
+        ("share/" + package_name + "/rviz", glob(os.path.join("rviz", "*.*"))),
+        ("share/" + package_name + "/meshes", glob(os.path.join("meshes", "*.*"))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,13 +29,12 @@ setup(
         ],
     },
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             "static_turtle_tf2_broadcaster = tf2_basic.static_turtle_tf2_broadcaster:main",
             "dynamic_turtle_tf2_broadcaster = tf2_basic.dynamic_turtle_tf2_broadcaster:main",
             "tf_listener = tf2_basic.tf_listener:main",
             "turtle_tf_listener = tf2_basic.turtle_tf_listener:main",
-            "follow_turtle = tf2_basic.follow_turtle:main"
-
+            "follow_turtle = tf2_basic.follow_turtle:main",
         ],
     },
 )
