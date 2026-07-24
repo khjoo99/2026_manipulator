b40826a18f271ce74ce5091c0fbb44c356efe70c
khjoo <khjoo990408@gmail.com>
2026-07-24 16:12:06 +0900

    urdf wheel

--- khj_ws/src/tf2_basic/launch/display.launch.py
+++ khj_ws/src/tf2_basic/launch/display.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
         [FindPackageShare("tf2_basic"), "rviz", "urdf.rviz"]
     )
     default_model_path = PathJoinSubstitution(
-        [FindPackageShare("tf2_basic"), "urdf", "02_leg_joint.urdf"]
+        [FindPackageShare("tf2_basic"), "urdf", "03_wheel_joint.urdf"]
     )
     model = DeclareLaunchArgument(
         name="model", default_value=default_model_path, description="model path"
