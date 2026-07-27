41bbe8b105a358490d36a8cbf40a6cf5af162d40
khjoo <khjoo990408@gmail.com>
2026-07-27 16:58:20 +0900

    dance

--- khj_ws/src/tf2_basic/setup.py
+++ khj_ws/src/tf2_basic/setup.py
@@ -16,6 +16,7 @@ setup(
         ("share/" + package_name + "/urdf", glob(os.path.join("urdf", "*.*"))),
         ("share/" + package_name + "/rviz", glob(os.path.join("rviz", "*.*"))),
         ("share/" + package_name + "/meshes", glob(os.path.join("meshes", "*.*"))),
+        ("share/" + package_name + "/data",glob(os.path.join("data", "*.yaml")),),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
