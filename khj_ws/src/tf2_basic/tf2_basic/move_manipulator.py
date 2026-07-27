de1ceb236430127668ef4aacd8fed875d4a431ca
khjoo <khjoo990408@gmail.com>
2026-07-27 16:03:08 +0900

    action gripper value

--- khj_ws/src/tf2_basic/tf2_basic/move_manipulator.py
+++ khj_ws/src/tf2_basic/tf2_basic/move_manipulator.py
@@ -1,7 +1,9 @@
 import rclpy
-from control_msgs.action import GripperCommand
+from action_msgs.msg import GoalStatus
+from control_msgs.action import GripperCommand, GripperCommand_GetResult_Response
 from rclpy.action import ActionClient
 from rclpy.node import Node
+from rclpy.task import Future
 from sensor_msgs.msg import JointState
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
@@ -10,7 +12,7 @@ class Manipulator_pub(Node):
     def __init__(self):
         super().__init__("manipulator_pub")  # 노드 이름
         # timer 등록
-        self.create_timer(0.5, self.timer_callback)
+        self.create_timer(2, self.timer_callback)
         self.pub = self.create_publisher(JointTrajectory, "arm_controller/joint_trajectory", 10)
         self.gripper_client = ActionClient(self, GripperCommand, "/gripper_controller/gripper_cmd")
         self.joint_state_subscription = self.create_subscription(
@@ -20,7 +22,7 @@ class Manipulator_pub(Node):
         self.current_gripper_position = 0.0
         self.joint_state_received = False
         self.count = True
-        self.duration_sec = 1
+        self.duration_sec = 2
 
     def timer_callback(self):
         msg = JointTrajectory()
@@ -35,6 +37,7 @@ class Manipulator_pub(Node):
                 -0.26300971181849175,
                 -0.6258641614575486,
             ]
+            self.move_gripper(0.019)
             self.count = False
         else:
             point.positions = [
@@ -43,6 +46,7 @@ class Manipulator_pub(Node):
                 0.3969902881815083,
                 0.8941358385424522,
             ]
+            self.move_gripper(-0.01)
             self.count = True
         seconds = int(self.duration_sec)
         nanoseconds = int((self.duration_sec - seconds) * 1_000_000_000)
@@ -55,6 +59,38 @@ class Manipulator_pub(Node):
 
     def joint_callback(self, msg: JointState):
         self.current_joint_position = msg.position
+
+    def move_gripper(self, position: float, max_effort=10.0, timeout_sec=5.0):
+        if not self.gripper_client.wait_for_server(timeout_sec=timeout_sec):
+            self.get_logger().info("gripper_controller Action 서버를 찾지 못햇습니다.")
+        goal = GripperCommand.Goal()
+        goal.command.position = float(position)
+        goal.command.max_effort = float(max_effort)
+        send_goal_future = self.gripper_client.send_goal_async(goal)
+        send_goal_future.add_done_callback(self.goal_callback)
+
+    def goal_callback(self, future: Future):
+        self.goal_handle = future.result()  # type: ignore
+        self.get_result_future = self.goal_handle.get_result_async()  # type: ignore
+        self.get_result_future.add_done_callback(self.get_result_callback)
+
+    def feedback_callback(
+        self,
+        msg: GripperCommand.Impl.FeedbackMessage,
+    ):
+        feedback: GripperCommand.Feedback = msg.feedback
+        self.get_logger().info(f"{feedback.position}")
+
+    def get_result_callback(self, future: Future):
+        result: GripperCommand_GetResult_Response = (
+            future.result()  # type: ignore
+        )
+        if result.status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info(f"succeeded result: {result.result.position}")
+        elif result.status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().info("aborted!!")
+        elif result.status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().info("canceled!!")
 
 
 def main(args=None):
