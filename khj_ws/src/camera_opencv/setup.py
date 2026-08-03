f41c19e2c115c1cf20c470208b134a951ec0479f
khjoo <khjoo990408@gmail.com>
2026-08-03 13:59:47 +0900

    img compressed

--- khj_ws/src/camera_opencv/setup.py
+++ khj_ws/src/camera_opencv/setup.py
@@ -26,6 +26,8 @@ setup(
             ["img_show = camera_opencv.img_show:main",
             "img_write = camera_opencv.img_write:main",
             "img_pub = camera_opencv.img_pub:main",
+            "img_compressed_pub = camera_opencv.img_compressed_pub:main",
+            "img_sub = camera_opencv.img_sub:main",
             
             ],
         
